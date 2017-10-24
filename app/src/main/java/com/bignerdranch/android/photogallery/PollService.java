1411bd2eb38c8eafc368cf2f575436c32b9b1197
MA Dingjie <mdj2812@gmail.com>
2017-10-24 11:30:36 +0200

    Chapter 28 change polling interval to 15 minutes

--- app/src/main/java/com/bignerdranch/android/photogallery/PollService.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/PollService.java
@@ -25,7 +25,7 @@ import java.util.concurrent.TimeUnit;
 public class PollService extends IntentService {
     private static final String TAG = "PollService";
 
-    private static final long POLL_INTERVAL_MS = TimeUnit.MINUTES.toMillis(1);
+    private static final long POLL_INTERVAL_MS = TimeUnit.MINUTES.toMillis(15);
     
     public static Intent newIntent(Context context) {
         return new Intent(context, PollService.class);
